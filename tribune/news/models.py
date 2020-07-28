f97c6ef7a9940b5b7383c6cf20c55431a23d6833
Fred Ojure <fredojure@hotmail.com>
2020-07-28 17:57:31 +0300

    First commit

---
+++ tribune/news/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Editor(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+    class Meta:
+        ordering = ['first_name']
+
+class tags(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
---
+++ virtual/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\fredojure\documents\moringa-school-core\the-moringa-tribune\virtual\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
