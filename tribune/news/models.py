1a97369491a72cf57b09c8e201d8b8797b5a48bd
Fred Ojure <fredojure@hotmail.com>
2020-07-29 18:50:14 +0300

    After adding Pillow and writing code for uploading and displaying images

--- tribune/news/models.py
+++ tribune/news/models.py
@@ -26,6 +26,7 @@ class Article(models.Model):
     editor = models.ForeignKey(Editor, on_delete=models.DO_NOTHING)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.ImageField(upload_to = 'articles/', blank=True)
 
     @classmethod
     def todays_news(cls):
