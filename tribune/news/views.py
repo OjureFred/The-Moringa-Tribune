18888f948cf9e8f220afe2ca903d02625cfe1b82
Fred Ojure <fredojure@hotmail.com>
2020-07-29 13:01:29 +0300

    Updated views to display news articles

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
+from .models import Article
 
 # Create your views here.
 def welcome(request):
@@ -8,7 +9,8 @@ def welcome(request):
 
 def news_today(request):
     date = dt.date.today()
-    return render(request, 'all-news/today-news.html', {"date": date})
+    news = Article.todays_news()
+    return render(request, 'all-news/today-news.html', {"date": date, "news": news})
 
 def convert_dates(dates):
     #Function that gets the weekday number for the date
@@ -33,5 +35,6 @@ def past_days_news(request, past_date):
 
     if date == dt.date.today():
         return redirect(news_of_day)
-
-    return render(request, 'all-news/past-news.html', {"date": date})
+    
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html', {"date": date, "news": news})
